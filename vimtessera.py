6bf3d8c3695efc65e72ddae603d8eae5bad16a5e
Claudio Klingler <ck@realtime-projects.com>
2013-11-30 01:16:18 +0100

    Updated to tessera changes. prints out tesseras in new buffer

--- vimtessera.py
+++ vimtessera.py
@@ -12,8 +12,12 @@ class tessera:
     def list(self):
         self.files = self.te.ls()
         self.index = 0
+        vim.command(":e " + vim.eval("tempname()"))
         for t in self.files:
-            print(t.data())
+            ident = t.ident()
+            vim.current.buffer.append("%s %s" % ( ident['ident'], ident['title'] ) )
+        vim.command(":set nomodifiable")
+        vim.command(":set nomodified")
 
     def next(self):
         vim.command(":e %s" % self.files[self.index])
