ea543e71ae63e2ae9aea72471707008267d0fe29
Claudio Klingler <ck@realtime-projects.com>
2013-12-05 00:17:06 +0100

    TElist: Added headline

--- vimtessera.py
+++ vimtessera.py
@@ -7,12 +7,6 @@ from tessera import GitTessera
 from vim import current, buffers
 from ConfigParser import ConfigParser
 
-def _highlight():
-    vim.command(":hi def TEid ctermfg=darkblue")
-    vim.command(":hi def TEtitle ctermfg=darkgreen")
-    vim.command(":syn match TEid '^[^ ]\+'")
-    vim.command(":syn match TEtitle ' .\+$'")
-
 class tessera:
     def __init__(self, directory = "."):
         self.directory = directory
@@ -25,12 +19,7 @@ class tessera:
         self.files = self.te.ls()
         self.index = 0
         vim.command(":e " + vim.eval("tempname()"))
-        for t in self.files:
-            ident = t.ident()
-            vim.current.buffer.append("%s %s" % ( ident['ident'], ident['title'] ) )
-        vim.command(":w")
-        vim.command(":set nomodifiable")
-        _highlight()
+        _display(self.files)
         vim.command(":nmap <Enter> :py tessera.open()<Enter>")
 
     def next(self):
@@ -39,7 +28,32 @@ class tessera:
 
     def open(self):
         (row, col) = vim.current.window.cursor
-        vim.command(":e %s" % self.files[ row - 2 ].filename)
+        vim.command(":e %s" % self.files[ row - (len(_headline) + 1) ].filename)
         vim.command(":set nomodifiable")
         vim.command(":nmap q :bd<Enter>")
+
+
+def _highlight():
+    vim.command(":hi def TEid ctermfg=darkblue")
+    vim.command(":hi def TEtitle ctermfg=darkgreen")
+    vim.command(":syn match TEid '^[^ ]\+'")
+    vim.command(":syn match TEtitle ' .\+$'")
+
+_headline = [ "# Welcome to vim-tessera",
+              "# ",
+              "# place the cursor over a Tessera and press enter to open it",
+              "# "
+            ]
+
+def _display(files):
+    """ displays the {files} Tesserae in the current buffer
+    """
+    del vim.current.buffer[0:len(vim.current.buffer)]
+    vim.current.buffer[0:len(_headline)] = _headline
+    for t in files:
+        ident = t.ident()
+        vim.current.buffer.append("%s %s" % ( ident['ident'], ident['title'] ) )
+    vim.command(":w")
+    vim.command(":set nomodifiable")
+    _highlight()
 
